716dd706b6531ef3548a19f69804f554abce4ea2
Denis Garcia <dev@weightlessgames.com>
2018-07-01 16:58:31 -0600

    no message

--- scripts/vrconfig.js
+++ scripts/vrconfig.js
@@ -122,15 +122,16 @@ var scenes = {
 var myImage = new Image(200, 200);
 
 function onLoad() {
-  // vrView = new VRView.Player('#vrview', {
+  vrView = new VRView.Player('#vrview', {
   //   image: 'images/blank.png',
   //   preview: 'images/blank.png',
   //   is_stereo: true,
   //   is_autopan_off: true
-  // });
-
   myImage.src = 'images/SalaSmall.jpg';
   document.body.appendChild(myImage);
+  });
+
+
 
   console.log('Console Message');
 
